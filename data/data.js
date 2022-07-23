1a586dae72d193ff46bb7e35473d3fa37d7a92cb
Cristoper Yarleque <caypsaturno@gmail.com>
2022-07-23 18:45:08 -0500

    version final

---
+++ data/data.js
@@ -0,0 +1,349 @@
+const fotos = [
+    {
+        id: 1,
+        src: "img/galery/1.jpeg"
+    },
+    {
+        id: 2,
+        src: "img/galery/2.jpeg"
+    },
+    {
+        id: 3,
+        src: "img/galery/3.jpeg"
+    },
+    {
+        id: 4,
+        src: "img/galery/4.jpeg"
+    },
+    {
+        id: 5,
+        src: "img/galery/5.jpeg"
+    },
+    {
+        id: 6,
+        src: "img/galery/6.jpeg"
+    },
+    {
+        id: 7,
+        src: "img/galery/7.jpeg"
+    },
+    {
+        id: 8,
+        src: "img/galery/8.jpeg"
+    },
+    {
+        id: 9,
+        src: "img/galery/9.jpeg"
+    },
+    {
+        id: 10,
+        src: "img/galery/10.jpeg"
+    },
+    {
+        id: 11,
+        src: "img/galery/11.jpeg"
+    },
+    {
+        id: 12,
+        src: "img/galery/12.jpeg"
+    },
+    {
+        id: 13,
+        src: "img/galery/13.jpeg"
+    },
+    {
+        id: 14,
+        src: "img/galery/14.jpeg"
+    },
+    {
+        id: 15,
+        src: "img/galery/15.jpeg"
+    },
+    {
+        id: 16,
+        src: "img/galery/16.jpeg"
+    },
+    {
+        id: 17,
+        src: "img/galery/17.jpeg"
+    },
+    {
+        id: 18,
+        src: "img/galery/18.jpeg"
+    },
+    {
+        id: 19,
+        src: "img/galery/19.jpeg"
+    },
+    {
+        id: 20,
+        src: "img/galery/20.jpeg"
+    },
+    {
+        id: 21,
+        src: "img/galery/21.jpeg"
+    },
+    {
+        id: 22,
+        src: "img/galery/22.jpeg"
+    },
+    {
+        id: 23,
+        src: "img/galery/23.jpeg"
+    },
+    {
+        id: 24,
+        src: "img/galery/24.jpeg"
+    },
+    {
+        id: 25,
+        src: "img/galery/25.jpeg"
+    },
+    {
+        id: 26,
+        src: "img/galery/26.jpeg"
+    },
+    {
+        id: 27,
+        src: "img/galery/27.jpeg"
+    },
+    {
+        id: 28,
+        src: "img/galery/28.jpeg"
+    },
+    {
+        id: 29,
+        src: "img/galery/29.jpeg"
+    },
+    {
+        id: 30,
+        src: "img/galery/30.jpeg"
+    },
+    {
+        id: 31,
+        src: "img/galery/31.jpeg"
+    },
+    {
+        id: 32,
+        src: "img/galery/32.jpeg"
+    },
+    {
+        id: 33,
+        src: "img/galery/33.jpeg"
+    },
+    {
+        id: 34,
+        src: "img/galery/34.jpeg"
+    },
+    {
+        id: 35,
+        src: "img/galery/35.jpeg"
+    },
+    {
+        id: 36,
+        src: "img/galery/36.jpeg"
+    },
+    {
+        id: 37,
+        src: "img/galery/37.jpeg"
+    },
+    {
+        id: 38,
+        src: "img/galery/38.jpeg"
+    },
+    {
+        id: 39,
+        src: "img/galery/39.jpeg"
+    },
+    {
+        id: 40,
+        src: "img/galery/40.jpeg"
+    },
+    {
+        id: 41,
+        src: "img/galery/41.jpeg"
+    },
+    {
+        id: 42,
+        src: "img/galery/42.jpeg"
+    },
+    {
+        id: 43,
+        src: "img/galery/43.jpeg"
+    },
+    {
+        id: 44,
+        src: "img/galery/44.jpeg"
+    },
+    {
+        id: 45,
+        src: "img/galery/45.jpeg"
+    },
+    {
+        id: 46,
+        src: "img/galery/46.jpeg"
+    },
+    {
+        id: 47,
+        src: "img/galery/47.jpeg"
+    },
+    {
+        id: 48,
+        src: "img/galery/48.jpeg"
+    },
+    {
+        id: 49,
+        src: "img/galery/49.jpeg"
+    },
+    {
+        id: 50,
+        src: "img/galery/50.jpeg"
+    },
+    {
+        id: 51,
+        src: "img/galery/51.jpeg"
+    },
+    {
+        id: 52,
+        src: "img/galery/52.jpeg"
+    },
+    {
+        id: 53,
+        src: "img/galery/53.jpeg"
+    },
+    {
+        id: 54,
+        src: "img/galery/54.jpeg"
+    },
+    {
+        id: 55,
+        src: "img/galery/55.jpeg"
+    },
+    {
+        id: 56,
+        src: "img/galery/56.jpeg"
+    },
+    {
+        id: 57,
+        src: "img/galery/57.jpeg"
+    },
+    {
+        id: 58,
+        src: "img/galery/58.jpeg"
+    },
+    {
+        id: 59,
+        src: "img/galery/59.jpeg"
+    },
+    {
+        id: 60,
+        src: "img/galery/60.jpeg"
+    },
+    {
+        id: 61,
+        src: "img/galery/61.jpeg"
+    },
+    {
+        id: 62,
+        src: "img/galery/62.jpeg"
+    },
+    {
+        id: 63,
+        src: "img/galery/63.jpeg"
+    },
+    {
+        id: 64,
+        src: "img/galery/64.jpeg"
+    },
+    {
+        id: 65,
+        src: "img/galery/65.jpeg"
+    },
+    {
+        id: 66,
+        src: "img/galery/66.jpeg"
+    },
+    {
+        id: 67,
+        src: "img/galery/67.jpeg"
+    },
+    {
+        id: 68,
+        src: "img/galery/68.jpeg"
+    },
+    {
+        id: 69,
+        src: "img/galery/69.jpeg"
+    },
+    {
+        id: 70,
+        src: "img/galery/70.jpeg"
+    },
+    {
+        id: 71,
+        src: "img/galery/71.jpeg"
+    },
+    {
+        id: 72,
+        src: "img/galery/72.jpeg"
+    },
+    {
+        id: 73,
+        src: "img/galery/73.jpeg"
+    },
+    {
+        id: 74,
+        src: "img/galery/74.jpeg"
+    },
+    {
+        id: 75,
+        src: "img/galery/75.jpeg"
+    },
+    {
+        id: 76,
+        src: "img/galery/76.jpeg"
+    }
+]
+
+const videos = [
+    {
+        id: 1,
+        src: "https://mega.nz/embed/baYHnCib#kIzsVIyj2wzObOiC89-uGNkgpBxHzRCQcnilA0hVyhQ"
+    },
+    {
+        id: 2,
+        src: "https://mega.nz/embed/bWpi0b6T#XVgPCm2QR8u_vlKrtLiXU8g0O_4-3d5ow8CNuAW8tCs"
+    },
+    {
+        id: 3,
+        src: "https://mega.nz/embed/CX4WwSxJ#FoP78vtRwIf4T9suDqvMKYMndEyxcB5qUVmAgaY1Bf8"
+    },
+    {
+        id: 4,
+        src: "https://mega.nz/embed/PCQX0QgY#8Pzv0m7C-tg2J-r2jG7lhWot2E7TjeUDNHte-UNEiaE"
+    }
+]
+
+const bodyFotos = document.querySelector(".fotos")
+const bodyVideos = document.querySelector(".videos")
+
+let div = ""
+for(let i = 1 ; i < fotos.length; i++) {
+    div = div + `<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+    <div class="service-item bg-light overflow-hidden h-100">
+      <img class="img-fluid " src=${fotos[i].src} alt="${i}" />
+    </div>
+  </div>`
+}
+bodyFotos.innerHTML = div
+
+let div1 = ""
+for(let i = 0 ; i < videos.length; i++) {
+    div1 = div1 + `<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+    <div class="service-item bg-light overflow-hidden h-100 embed-responsive  embed-responsive-16by9">
+    <iframe frameborder="0" class=" embed-responsive-item" src=${videos[i].src}></iframe>
+    </div>
+  </div>`
+}
+
+bodyVideos.innerHTML = div1
